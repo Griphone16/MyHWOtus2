93e11771bcc07ce8e3ed46332cf5569e130d28b8
Griphone16 <Griphone@mail.ru>
2022-07-25 23:57:02 +0300

    Добавлены тесты на вопросы и ответы

---
+++ src/test/java/com/hw/myhw1/dao/ReaderCSVDaoSimpleTest.java
@@ -0,0 +1,44 @@
+package com.hw.myhw1.dao;
+
+import com.hw.myhw1.domain.Answer;
+import com.hw.myhw1.domain.Question;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReaderCSVDaoSimpleTest {
+
+
+    public static final String FILE_CSV = "FileCSV";
+
+    @Test
+    @DisplayName("Должен получать имя файла CSV")
+    void getFileCSV() {
+        ReaderCSVDaoSimple readerCSVDaoSimple = new ReaderCSVDaoSimple();
+        readerCSVDaoSimple.setFileCSV(FILE_CSV);
+
+        assertEquals(readerCSVDaoSimple.getFileCSV(), FILE_CSV);
+    }
+
+    @Test
+    @DisplayName("Должен устанавливать имя файла CSV")
+    void setFileCSV() {
+        ReaderCSVDaoSimple readerCSVDaoSimple = new ReaderCSVDaoSimple();
+        readerCSVDaoSimple.setFileCSV(FILE_CSV);
+        
+        assertEquals(readerCSVDaoSimple.getFileCSV(), FILE_CSV);
+    }
+
+    @Test
+    @DisplayName("Должен считывать вопросы из файла CSV")
+    void getAnswers() {
+        ReaderCSVDaoSimple readerCSVDaoSimple = new ReaderCSVDaoSimple();
+        readerCSVDaoSimple.setFileCSV("E:/My HW Otus/HW.1/myHW1/src/main/resources/QuestionsTest.csv");
+        List<Question> questions = readerCSVDaoSimple.getAnswers();
+        assertEquals(questions.size(), 2);
+
+    }
+}
